b077be7b23a7783afb5954977b74d0d547ba87e2
manyajamwal <128244824+manyajamwal@users.noreply.github.com>
2024-06-01 13:35:38 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Manya Jamwal",
+  title: "IT Engineering Student",
+  email: "manyajamwal13@gmail.com",
+  gitHub: "https://github.com/manyajamwal",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/manya-jamwal/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
